984fcec6ee6caa403036be21e22ae30abce51dcd
dgleiche <dgleiche@me.com>
2016-11-26 03:22:54 -0500

    Unicorrrrrn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  def page_not_found
+    e = Error.new(:status => 404, :message => 'Invalid request')
+    render :json => e.to_json, :status => 404
+  end
 end
