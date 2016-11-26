3e55db3feb07a84f69685224ff4dd81185b22dce
dgleiche <dgleiche@me.com>
2016-11-26 03:25:13 -0500

    Some flair

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def page_not_found
-    e = Error.new(:status => 404, :message => 'Invalid request')
+    e = Error.new(:status => 404, :message => 'Invalid request. \'Pac down!')
     render :json => e.to_json, :status => 404
   end
 end
